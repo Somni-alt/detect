cbaecb53247de4e7439b771aeb6824d3d480446a
Somni-alt <arthur@somni.paris>
2025-08-08 18:01:29 +0200

    Clean6

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,14 @@
-import {getRequestConfig, requestLocale} from 'next-intl/server';
-export default getRequestConfig(async () => {
-  const locale = await requestLocale();
-  return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default
-  };
+import {getRequestConfig} from 'next-intl/server';
+export default getRequestConfig(async ({locale}) => {
+  try {
+    return {
+      locale,
+      messages: (await import(`../messages/${locale}.json`)).default
+    };
+  } catch {
+    return {
+      locale: 'en',
+      messages: (await import(`../messages/en.json`)).default
+    };
+  }
 });
